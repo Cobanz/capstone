52ec8210892adfbdacde52f1a8e676284cd96622
Charles Grant Nichols <grant@inkdots.com>
2021-06-23 15:17:58 -0500

    wierd loop

--- client/src/Score/ScoresContainer.js
+++ client/src/Score/ScoresContainer.js
@@ -1,91 +1,90 @@
-import React, {useState,useEffect} from 'react'
+import React from 'react'
 import { MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
-import { useParams } from 'react-router';
 // import { useState } from 'react';
 
 
 
-class ScoresContainer extends React.Component{
-    state={
-        scores:[],
-        name:'',
-        score:'',
-        id:'',
-        role:''
+class ScoresContainer extends React.Component {
+    state = {
+        scores: [],
+        name: '',
+        score: '',
+        id: '',
+        role: ''
     }
-  
-
-   componentDidMount(){
-    fetch('/me')
-    .then(res=> res.json())
-    .then(data => this.setState({
-        id: data.id,
-        role: data.role
-    }))
-   
 
 
-     
-   }
+    componentDidMount() {
+        fetch('/me')
+            .then(res => res.json())
+            .then(data => this.setState({
+                id: data.id,
+                role: data.role
+            }))
 
-   getScores=()=>{
-            const userId= this.state.id
-            if(this.state.scores.length<1){
-                if(this.state.role=='admin'){
+
+
+
+    }
+
+    getScores = () => {
+        const userId = this.state.id
+        if (this.state.scores.length < 1) {
+            if (this.state.role === 'admin') {
                 console.log('im admin')
-                  fetch(`/score`)
-                .then(res=> res.json())
-                .then(data => this.setState({
-                    scores: data
-                }))
+                fetch(`/score`)
+                    .then(res => res.json())
+                    .then(data => this.setState({
+                        scores: data
+                    }))
 
-            }else{
+            } else {
                 console.log('im Not admin')
                 fetch(`/score/${userId}`)
-                    .then(res=> res.json())
+                    .then(res => res.json())
                     .then(data => this.setState({
                         scores: data
                     }))
             }
-   }
-   }
+        }
+    }
 
 
 
     render() {
-        let row=1
+        let row = 1
 
         return (
             <div>
                 {/*put ()  after getScore */}
                 {this.getScores()}
-            <MDBTable>
-      <MDBTableHead>
-        <tr>
-          <th scope='col'>#</th>
-          <th scope='col'>Username</th>
-          <th scope='col'>Score</th>
-        </tr>
-      </MDBTableHead>
-      <MDBTableBody>
-      
+                <MDBTable>
+                    <MDBTableHead>
+                        <tr>
+                            <th scope='col'>#</th>
+                            <th scope='col'>Username</th>
+                            <th scope='col'>Score</th>
+                        </tr>
+                    </MDBTableHead>
+                    <MDBTableBody>
 
-      {this.state.scores.map(score=>{
-                return(
-            <tr>
-                <th>{row++}</th>
-                <td>{score.user.name}</td>
-                <td>{score.score}</td>
-                {this.state.role==='admin' ? <td>delete</td>:null}
-            </tr>
-                )
-            })}
-            
-      </MDBTableBody>
-    </MDBTable>
-    </div>
-    )
-}
+
+                        {this.state.scores.map(score => {
+                            return (
+                                <tr>
+                                    <th>{row++}</th>
+                                    <td>{score.user.name}</td>
+                                    <td>{score.score}</td>
+                                    {this.state.role === 'admin' ? <td>delete</td> : null}
+                                </tr>
+                            )
+                        })}
+
+                    </MDBTableBody>
+                </MDBTable>
+            </div>
+        )
+    }
 }
 
-export default ScoresContainer 
+export default ScoresContainer
